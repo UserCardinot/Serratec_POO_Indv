40194193556545343b566394bd71e67862a4b385
UserCardinot <lucas.cardinot@outlook.com>
2024-09-16 18:20:45 -0300

    add arquivo.jar e remocao do jar no gitignore

--- ProjetoIndv/src/br/com/projeto_indv/individuos/Professor.java
+++ ProjetoIndv/src/br/com/projeto_indv/individuos/Professor.java
@@ -1,8 +1,5 @@
 package br.com.projeto_indv.individuos;
 
-import br.com.projeto_indv.materias.Experimental;
-import br.com.projeto_indv.materias.Pratica;
-import br.com.projeto_indv.materias.Teorica;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -78,19 +75,7 @@ public class Professor {
     public static Map<String, Professor> getMapaProf() {
         return getMapaProf;
     }
-
-    public void addMateria(Experimental materia) {
-        getMapaProf().put(materia.getNomeMateria(), this);
-    }
-
-    public void addMateria(Teorica materia) {
-        getMapaProf().put(materia.getNomeMateria(), this);
-    }
-
-    public void addMateria(Pratica materia) {
-        getMapaProf().put(materia.getNomeMateria(), this);
-    }
-
+    
     @Override
     public String toString() {
         return "Professor{" + "id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", email=" + email + ", RegistroAcademico=" + RegistroAcademico + '}';
